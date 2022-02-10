479d3c20a631c7d2ec4ce69f495a172c80107ceb
Tadeo Guerstein <tadeleonino@hotmail.com>
2022-02-09 21:52:05 -0300

    Added hooks for getting data from the weatherapp api

--- src/pages/CityPage.jsx
+++ src/pages/CityPage.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
+import convertUnits from 'convert-units';
 import Grid from '@material-ui/core/Grid';
+import moment from 'moment';
+import { useParams } from 'react-router-dom';
 import CityInfo from '../components/CityInfo';
 import Weather from '../components/Weather';
 import WeatherDetails from '../components/WeatherDetails';
@@ -8,66 +12,120 @@ import Forecast from '../components/Forecast';
 import AppFrame from '../components/AppFrame';
 import Paper from '@material-ui/core/Paper';
 
+const forecastItemListExample = [
+    {
+        weekDay: "Tuesday", 
+        hour: 10,
+        state: "clouds",
+        temperature: 22
+    },
+    {
+        weekDay: "Wednesday", 
+        hour: 22,
+        state: "clear",
+        temperature:33
+    },
+    {
+        weekDay: "Thursday", 
+        hour: 12,
+        state: "rain",
+        temperature: 25
+    }
+]
+
+const dataExample = [
+    {
+        "dayHour": "Jue 18",
+        "min": 14,
+        "max": 22,
+    },
+    {
+        "dayHour": "Vie 06",
+        "min": 18,
+        "max": 27,
+    },
+    {
+        "dayHour": "Vie 12",
+        "min": 18,
+        "max": 28,
+    },
+    {
+        "dayHour": "Vie 18",
+        "min": 18,
+        "max": 25,
+    },
+    {
+        "dayHour": "Sab 06",
+        "min": 15,
+        "max": 22,
+    },
+    {
+        "dayHour": "Sab 12",
+        "min": 12,
+        "max": 19,
+    }
+]
+
 const CityPage = () => {
+    const [data, setData] = useState(null);
+    const [forecastItemList, setforecastItemList] = useState(null);
+
+    const { city, countryCode } = useParams(); //* trae y permite usar los parametros de la url
+
+    useEffect(() => {
+        const getForecast = async () => {
+            const apiId = "e90c52a68c04c11a7cfb6bfd8a8a4a8b";
+            const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city},${countryCode}&appid=${apiId}`
+            try {
+                const { data } = await axios.get(url)
+
+                console.log("data:", data);
+
+                const toCelsius = (temp) => convertUnits(temp).from("K").to("C").toFixed(0)
+                const daysAhead = [0,1,2,3,4,5];
+                const days = daysAhead.map(day => moment().add(day, 'd'))
+                const dataAux = days.map(day => {
+                    const tempObjArray = data.list.filter(item => {
+                        const dayOfYear = moment.unix(item.dt).dayOfYear()
+                        return dayOfYear === day.dayOfYear()
+                    })
+
+                    const temps = tempObjArray.map(item => item.main.temp)
+                    
+                    // dayHour, min, max
+                    return ({
+                        dayHour: day.format("ddd"),
+                        min: toCelsius(Math.min(...temps)),
+                        max: toCelsius(Math.max(...temps))
+                    })
+                })
+                setData(dataAux)
+
+                const interval = [4,6,8,12,14,16,20,22,24]
+                const forecastItemListAux = data.list
+                .filter((item, index) => interval.includes(index))
+                .map(item => {
+                    return ({
+                        hour: moment.unix(item.dt).hour(),
+                        weekDay: moment.unix(item.dt).format("dddd"),
+                        state: item.weather[0].main.toLowerCase(),
+                        temperature: toCelsius(item.main.temp)
+                    })
+                })
+                setforecastItemList(forecastItemListAux)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getForecast()
+    }, [city, countryCode]);
+    
     const country = "Argentina";
-    const city = "Buenos Aires";
-    const state = "cloudy";
+    const state = "clouds";
     const temperature = 10;
     const humidity = 40;
     const wind = 20;
-    const forecastItemList = [
-        {
-            weekDay: "Tuesday", 
-            hour: 10,
-            state: "clouds",
-            temperature: 22
-        },
-        {
-            weekDay: "Wednesday", 
-            hour: 22,
-            state: "clear",
-            temperature:33
-        },
-        {
-            weekDay: "Thursday", 
-            hour: 12,
-            state: "rain",
-            temperature: 25
-        }
-    ]
-
-    const data = [
-        {
-            "dayHour": "Jue 18",
-            "min": 14,
-            "max": 22,
-        },
-        {
-            "dayHour": "Vie 06",
-            "min": 18,
-            "max": 27,
-        },
-        {
-            "dayHour": "Vie 12",
-            "min": 18,
-            "max": 28,
-        },
-        {
-            "dayHour": "Vie 18",
-            "min": 18,
-            "max": 25,
-        },
-        {
-            "dayHour": "Sab 06",
-            "min": 15,
-            "max": 22,
-        },
-        {
-            "dayHour": "Sab 12",
-            "min": 12,
-            "max": 19,
-        }
-    ]
+    
 
     return( 
         <AppFrame>
@@ -89,11 +147,15 @@ const CityPage = () => {
                 </Grid>
 
                 <Grid item>
-                    <ForecastChart data={data}/>
+                    {
+                        data && <ForecastChart data={data}/>
+                    }
                 </Grid>
 
                 <Grid item>
-                    <Forecast forecastItemList={forecastItemList}/>
+                    {
+                        forecastItemList && <Forecast forecastItemList={forecastItemList}/>
+                    }
                 </Grid>
 
                 </Grid>
