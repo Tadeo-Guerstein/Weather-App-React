fff84fa099a7c55d7a78f6ec347289d30a789f77
Tadeo Guerstein <tadeleonino@hotmail.com>
2022-02-09 21:49:58 -0300

    Changed the city route path

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
                     <MainPage/>
                 </Route>
 
-                <Route path="/city">
+                <Route path="/city/:countryCode/:city">
                     <CityPage/>
                 </Route>
 
