2c3b3df5a50b1aa3083f9fc761e5c6b06ea6f8f2
Tadeo Guerstein <tadeleonino@hotmail.com>
2022-02-12 09:55:35 -0300

    created functions for the state and useEffect hooks so when rendering it doesn't call a lot of times to the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import WelcomePage from './pages/WelcomePage';
 import CityPage from './pages/CityPage';
@@ -6,6 +6,30 @@ import MainPage from './pages/MainPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 const App = () => {
+    const [allWeather, setAllWeather] = useState({});
+    const [chartData, onSetChartData] = useState(null);
+    const [forecastItemList, onSetforecastItemList] = useState(null);
+
+    const onSetAllWeather = useCallback((weathercity) => {
+            setAllWeather(allWeather => {
+                return ({ ...allWeather, ...weathercity})
+            })
+        }, [setAllWeather])
+    
+    const actions = useMemo(() => (
+        {
+            onSetAllWeather, 
+            onSetChartData, 
+            onSetforecastItemList
+        }
+    ), [onSetAllWeather, onSetChartData, onSetforecastItemList])
+    
+    const data = useMemo(() => (
+        {
+            allWeather, chartData, forecastItemList
+        }
+    ), [allWeather, chartData, forecastItemList])
+
     return (
         <Router>
             <Switch>
@@ -14,11 +38,11 @@ const App = () => {
                 </Route>
 
                 <Route path="/main">
-                    <MainPage/>
+                    <MainPage data={data} actions={actions}/>
                 </Route>
 
                 <Route path="/city/:countryCode/:city">
-                    <CityPage/>
+                    <CityPage data={data} actions={actions}/>
                 </Route>
 
                 <Route>
